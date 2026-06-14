e16721842b92ed272f92f06e5170703d7ce0806e
heyneeff <neeffneeff@gmail.com>
2026-06-13 19:37:07 -0600

    Add migration for events.city, ticket_price, image_url columns (missing from schema)

---
+++ migration_event_city_price.sql
@@ -0,0 +1,3 @@
+alter table events add column if not exists city text;
+alter table events add column if not exists ticket_price numeric;
+alter table events add column if not exists image_url text;
